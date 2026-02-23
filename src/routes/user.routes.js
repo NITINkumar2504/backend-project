c30d1d22c9e1a8b77e9c8edb3e95061d13e8fb76
Nitin Kumar <nk9465110@gmail.com>
2026-02-23 15:30:02 +0530

    add user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getUserWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,6 +23,13 @@ router.route('/login').post(loginUser)
 // secured routes: user must be logged in for these routes
 router.route('/logout').post(verifyJWT, logoutUser)   // verifyJWT is middleware
 router.route('/refreshToken').post(refreshAccessToken)
+router.route('/changePassword').post(verifyJWT, changeCurrentPassword)
+router.route('/currentUser').get(verifyJWT, getCurrentUser)
+router.route('/updateDetails').patch(verifyJWT, updateAccountDetails)   // not post create new resource, patch update a portion of a resource
+router.route('/updateAvatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar)
+router.route('/updateCoverImage').patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage)
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile)
+router.route('/watchHistory').get(verifyJWT, getUserWatchHistory)
 
 
 
